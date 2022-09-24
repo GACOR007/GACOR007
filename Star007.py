ba1413f1210f5284acb2fd702605a58a40a04f93
GACOR007 <110379992+GACOR007@users.noreply.github.com>
2022-09-24 12:27:21 +0700

    Create Star007.py

---
+++ Star007.py
@@ -0,0 +1,2 @@
+
+https://idn2-sg40.ppgames.net/gs2c/html5Game.do?jackpotid=1&gname=Starlight%20Princess&extGame=1&ext=0&cb_target=exist_tab&symbol=vs20starlight&jurisdictionID=99&minilobby=false&mgckey=AUTHTOKEN@3fdecab1acae196551c631f0f4e7b3084f23a17484326617b2d307ff654051df~stylename@idn2_idntotob2b_7~SESSION@73c16c38-00cf-47eb-9412-2fac769243ee~SN@7b14496e&tabName=
